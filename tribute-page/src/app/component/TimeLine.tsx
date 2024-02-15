27f79dadf4b54dfbdcf3f3f71fb7fed47565e0d7
Osita <chrisjerry070@gmail.com>
2024-02-15 22:55:26 +0400

    trying to create a component for time line but currently facing a bug will resolve i will continue tomorrow

--- tribute-page/src/app/component/TimeLine.tsx
+++ tribute-page/src/app/component/TimeLine.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import data from "../../data.json";
 
- const TimeLine = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+interface arrayType {
+  year : string;
+  article : string;
 }
 
-export default TimeLine
+interface quotesType {
+  quote : string
+}
+interface dataType {
+    youth : arrayType[];
+    activism : arrayType[];
+    legacy : arrayType[];
+    quotes : quotesType[]
+}
+
+
+interface timeType {
+   text : string;
+   location : dataType;
+   array : arrayType[]
+}
+
+const TimeLine = ({text, location, array} : timeType) => {
+  return (
+    <div>
+      <ul className="">
+        <p className=" bg-black w-[95%] text-center mx-auto py-1 text-lg rounded-xl my-2">
+          {text}
+        </p>
+        {/* {data.location.map((item : arrayType ) => (
+          <li className=" list-disc text-sm w-[85%] mx-auto">
+            <span>{item.year}</span>
+            <span>{item.article}</span>
+          </li>
+        ))} */}
+      </ul>
+    </div>
+  );
+};
+
+export default TimeLine;
